dd3dc61061b4a3281a6cca249abe6518cc0daf15
Ion V. Tataru <ion.v.tataru@outlook.com>
2023-12-06 22:27:37 +0200

    Associate the imported SSH key with the bastion
    Commit fixes the name of the SSH key (to reflect the Vultr config) and
    associates it with the bastion_host while also ensuring that any
    changes made to the key (as happened during the import work) do not
    result in a direct replacement for the server - that will be reserved to
    the planned OS upgrade.

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -7,10 +7,16 @@ resource "vultr_instance" "bastion_host" {
   hostname          = "ssh"
   tags              = ["bastion_host", "jump_host"]
   backups           = "disabled"
+  enable_ipv6       = false
+  ssh_key_ids       = [vultr_ssh_key.rtsa_ssh_key.id]
+
+  lifecycle {
+    ignore_changes = [ssh_key_ids]
+  }
 }
 
 resource "vultr_ssh_key" "rtsa_ssh_key" {
-  name    = "rtsa-ssh-key"
+  name    = "rtsa_ssh_key"
   ssh_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC+ao9nJS+22c0N4jYNxL+We5DAfOQcLKlymp4FJ4+v1XfztfoqdY6U7ufl4U6vMROmqJUS6RCNqrAh3Iqi7LZlBok8RiwK//IQWKv8gqjoiHP5Cocoy0Qqhd77rAU3ncOn09Fzj9RD9UGaQpxmfZk+0/6euyCQeGGYNpKNE9bj5SuOF+VFzfOIpgk4Zhy0dbnJ7G//WPWS0ecaqWb1KsgnAUuKzpjX0l9ta+/gr0uvg9W19T4CRocDdrEDeZULPkowoifXQ5G0bGEfb8o74JVwx0GdQk4dhqRZ7/ovIAhUCzJqabccPqdfzXyfC5dDIxRUEeXqmx852ezH34JJFlwCfbVUYNdGWYL4fgQK/L0KQfEO4kRaZV9JGmT4F8Peyns7YcdHCouuyB5hDW+qg9lhPQoFsSkD3WMkzAj2moea4L9UijcviigZWfYZ/4KIlg9krkYQ995SKG86ACNr7VpVOOEw05g1jJvlHYLZsXDV6iu6/96m585kcsmVoxkgYxkS4XSKIcJ8kggxcAW56z2laqsKmsIIzoF+Q/ILYm+zM9HxEHA4F57fi7Uu29PozbUPBYFs4NIYiuBD712euro6sSuhMiAmLBQ6QYx6zMsAEJnWgSXRN2DX4bmAwGhCkDSRr426CSMWRBKK+7ZhakQtGlGUCJkBUZi1sgQhNfTlOQ== RTSA"
 
 }
