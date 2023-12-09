9f9844595421aa5c2406ce06ef66e302b87ebd12
Ion V. Tataru <ion.v.tataru@outlook.com>
2023-12-10 00:29:53 +0200

    Use the newly declared vars in the resource defs

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,11 +1,11 @@
 resource "vultr_instance" "bastion_host" {
-  plan              = "vc2-1c-0.5gb"
-  region            = "ewr"
-  os_id             = 2187 #OpenBSD 7.4 x64
+  plan              = var.vultr_api_instance_args["bastion"].plan_id
+  region            = var.vultr_api_instance_args["bastion"].region_id
+  os_id             = var.vultr_api_instance_os["bastion"].os_id
   firewall_group_id = "4af0bd5a-4164-44b8-8a26-6f81acdfc4f5"
-  label             = "bastion"
-  hostname          = "bastion"
-  tags              = ["bastion_host", "jump_host"]
+  hostname          = var.vultr_api_instance_args["bastion"].hostname
+  label             = var.vultr_api_instance_args["bastion"].hostname
+  tags              = var.vultr_api_instance_args["bastion"].tags
   backups           = "disabled"
   enable_ipv6       = false
   ssh_key_ids       = [vultr_ssh_key.rtsa_ssh_key.id]
@@ -16,13 +16,13 @@ resource "vultr_instance" "bastion_host" {
 }
 
 resource "vultr_instance" "webserver01" {
-  plan              = "vhp-1c-1gb-amd"
-  region            = "lhr"
-  os_id             = 2136 #Debian 12
+  plan              = var.vultr_api_instance_args["webserver01"].plan_id
+  region            = var.vultr_api_instance_args["webserver01"].region_id
+  os_id             = var.vultr_api_instance_os["webserver01"].os_id
   firewall_group_id = "af3a8c3a-5e59-4988-abbd-9ebf8e0dab36"
-  label             = "webserver01"
-  hostname          = "webserver01"
-  tags              = ["webserver", "www"]
+  hostname          = var.vultr_api_instance_args["webserver01"].hostname
+  label             = var.vultr_api_instance_args["webserver01"].hostname
+  tags              = var.vultr_api_instance_args["webserver01"].tags
   backups           = "disabled"
   enable_ipv6       = false
   ssh_key_ids       = [vultr_ssh_key.rtsa_ssh_key.id]
@@ -33,13 +33,13 @@ resource "vultr_instance" "webserver01" {
 }
 
 resource "vultr_instance" "webserver02" {
-  plan              = "vhp-1c-1gb-amd"
-  region            = "icn"
-  os_id             = 1869 #Rocky Linux 9
+  plan              = var.vultr_api_instance_args["webserver02"].plan_id
+  region            = var.vultr_api_instance_args["webserver02"].region_id
+  os_id             = var.vultr_api_instance_os["webserver02"].os_id
   firewall_group_id = "af3a8c3a-5e59-4988-abbd-9ebf8e0dab36"
-  label             = "webserver02"
-  hostname          = "webserver02"
-  tags              = ["webserver", "www"]
+  hostname          = var.vultr_api_instance_args["webserver02"].hostname
+  label             = var.vultr_api_instance_args["webserver02"].hostname
+  tags              = var.vultr_api_instance_args["webserver02"].tags
   backups           = "disabled"
   enable_ipv6       = false
   ssh_key_ids       = [vultr_ssh_key.rtsa_ssh_key.id]
