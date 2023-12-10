9611d0b244965da9f80c99f111fe3a362381d5b1
Ion V. Tataru <ion.v.tataru@outlook.com>
2023-12-10 15:47:33 +0200

    Create Vultr FW rules for the TF managed hosts
    The FW rules enforce the point of using the bastion for SSH access to
    the webservers, open the HTTP(S) ports on the latter and allow ICMP
    across all the hosts (bastion and webservers); not in use yet, the OS
    of the hosts will first need to be configured via Ansible.

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -61,3 +61,69 @@ resource "vultr_ssh_key" "rtsa_ssh_key" {
     prevent_destroy = true
   }
 }
+
+resource "vultr_firewall_group" "bastion_host_fwgrp" {
+  description = "Firewall group for the bastion host, will fully open port 22/TCP and allow ICMP"
+}
+
+resource "vultr_firewall_rule" "bastion_host_allow_ssh" {
+  firewall_group_id = vultr_firewall_group.bastion_host_fwgrp.id
+  protocol          = "tcp"
+  ip_type           = "v4"
+  subnet            = "0.0.0.0"
+  subnet_size       = 0
+  port              = "22"
+  notes             = "Open port 22/TCP to the Internet"
+}
+
+resource "vultr_firewall_rule" "bastion_host_allow_icmp" {
+  firewall_group_id = vultr_firewall_group.bastion_host_fwgrp.id
+  protocol          = "icmp"
+  ip_type           = "v4"
+  subnet            = "0.0.0.0"
+  subnet_size       = 0
+  notes             = "Allow ICMP messages from anywhere"
+}
+
+resource "vultr_firewall_group" "webservers_fwgrp" {
+  description = "Firewall group for the webservers, will fully open ports [80,443]/TCP, open port 22/TCP for the bastion host only and allow ICMP"
+}
+
+resource "vultr_firewall_rule" "webservers_restrict_ssh" {
+  firewall_group_id = vultr_firewall_group.webservers_fwgrp.id
+  protocol          = "tcp"
+  ip_type           = "v4"
+  subnet            = vultr_instance.bastion_host.main_ip
+  subnet_size       = 32
+  port              = "22"
+  notes             = "Open port 22/TCP only for the bastion host"
+}
+
+resource "vultr_firewall_rule" "webservers_allow_http" {
+  firewall_group_id = vultr_firewall_group.webservers_fwgrp.id
+  protocol          = "tcp"
+  ip_type           = "v4"
+  subnet            = "0.0.0.0"
+  subnet_size       = 0
+  port              = "80"
+  notes             = "Open port 80/TCP to the Internet"
+}
+
+resource "vultr_firewall_rule" "webservers_allow_https" {
+  firewall_group_id = vultr_firewall_group.webservers_fwgrp.id
+  protocol          = "tcp"
+  ip_type           = "v4"
+  subnet            = "0.0.0.0"
+  subnet_size       = 0
+  port              = "443"
+  notes             = "Open port 443/TCP to the Internet"
+}
+
+resource "vultr_firewall_rule" "webservers_allow_icmp" {
+  firewall_group_id = vultr_firewall_group.webservers_fwgrp.id
+  protocol          = "icmp"
+  ip_type           = "v4"
+  subnet            = "0.0.0.0"
+  subnet_size       = 0
+  notes             = "Allow ICMP messages from anywhere"
+}
