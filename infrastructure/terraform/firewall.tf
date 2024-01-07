18514ececd56b1f74c07a1501e5d338ade772f18
Ion V. Tataru <ion.v.tataru@outlook.com>
2024-01-07 17:03:08 +0200

    Make the vultr_firewall_* conf easier to parse

--- infrastructure/terraform/firewall.tf
+++ infrastructure/terraform/firewall.tf
@@ -1,65 +1,74 @@
-resource "vultr_firewall_group" "bastion_host_fwgrp" {
-  description = "Firewall group for the bastion host, will fully open port 22/TCP and allow ICMP"
+resource "vultr_firewall_group" "bastion" {
+  description = <<-EOT
+    Firewall group for the bastion, meant to:
+    - open SSH port to any
+    - allow ICMP from any
+  EOT
 }
 
-resource "vultr_firewall_rule" "bastion_host_allow_ssh" {
-  firewall_group_id = vultr_firewall_group.bastion_host_fwgrp.id
+resource "vultr_firewall_rule" "bastion_allow_ssh" {
+  firewall_group_id = vultr_firewall_group.bastion.id
   protocol          = "tcp"
   ip_type           = "v4"
   subnet            = "0.0.0.0"
   subnet_size       = 0
   port              = "22"
-  notes             = "Open port 22/TCP to the Internet"
+  notes             = "pf: pass in inet proto tcp port 22 from any"
 }
 
-resource "vultr_firewall_rule" "bastion_host_allow_icmp" {
-  firewall_group_id = vultr_firewall_group.bastion_host_fwgrp.id
+resource "vultr_firewall_rule" "bastion_allow_icmp" {
+  firewall_group_id = vultr_firewall_group.bastion.id
   protocol          = "icmp"
   ip_type           = "v4"
   subnet            = "0.0.0.0"
   subnet_size       = 0
-  notes             = "Allow ICMP messages from anywhere"
+  notes             = "pf: pass in inet proto icmp from any"
 }
 
-resource "vultr_firewall_group" "webservers_fwgrp" {
-  description = "Firewall group for the webservers, will fully open ports [80,443]/TCP, open port 22/TCP for the bastion host only and allow ICMP"
+resource "vultr_firewall_group" "webservers" {
+  description = <<-EOT
+    Firewall group for the webservers, meant to:
+    - open HTTP and HTTPS ports to any
+    - open SSH port to <bastion>
+    - allow ICMP from any
+  EOT
 }
 
 resource "vultr_firewall_rule" "webservers_restrict_ssh" {
-  firewall_group_id = vultr_firewall_group.webservers_fwgrp.id
+  firewall_group_id = vultr_firewall_group.webservers.id
   protocol          = "tcp"
   ip_type           = "v4"
   subnet            = vultr_instance.host["bastion"].main_ip
   subnet_size       = 32
   port              = "22"
-  notes             = "Open port 22/TCP only for the bastion host"
+  notes             = "pf: pass in inet proto tcp port 22 from <bastion>"
 }
 
 resource "vultr_firewall_rule" "webservers_allow_http" {
-  firewall_group_id = vultr_firewall_group.webservers_fwgrp.id
+  firewall_group_id = vultr_firewall_group.webservers.id
   protocol          = "tcp"
   ip_type           = "v4"
   subnet            = "0.0.0.0"
   subnet_size       = 0
   port              = "80"
-  notes             = "Open port 80/TCP to the Internet"
+  notes             = "pf: pass in inet proto tcp port 80 from any"
 }
 
 resource "vultr_firewall_rule" "webservers_allow_https" {
-  firewall_group_id = vultr_firewall_group.webservers_fwgrp.id
+  firewall_group_id = vultr_firewall_group.webservers.id
   protocol          = "tcp"
   ip_type           = "v4"
   subnet            = "0.0.0.0"
   subnet_size       = 0
   port              = "443"
-  notes             = "Open port 443/TCP to the Internet"
+  notes             = "pf: pass in inet proto tcp port 443 from any"
 }
 
 resource "vultr_firewall_rule" "webservers_allow_icmp" {
-  firewall_group_id = vultr_firewall_group.webservers_fwgrp.id
+  firewall_group_id = vultr_firewall_group.webservers.id
   protocol          = "icmp"
   ip_type           = "v4"
   subnet            = "0.0.0.0"
   subnet_size       = 0
-  notes             = "Allow ICMP messages from anywhere"
+  notes             = "pf: pass in inet proto icmp from any"
 }
