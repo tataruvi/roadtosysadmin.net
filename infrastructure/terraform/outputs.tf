524bf07d2325b3c958b136ea173bababf1cec07c
Ion V. Tataru <ion.v.tataru@outlook.com>
2024-01-14 15:15:30 +0200

    Set up TF integration with an Ansible dynamic inv
    Also modify .gitignore accordingly and improve the name of the existing
    output block.

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -1,7 +1,27 @@
-output "host_ip_address" {
-  description = "Map managed hosts to their corresponding IPv4 addresses"
+output "hosts_to_ip_addr" {
+  description = <<-EOT
+    Map the hostnames of managed hosts to their corresponding IPv4 addresses
+  EOT
+
   value = {
     for instance, attributes in vultr_instance.host :
     instance => attributes.main_ip
   }
+}
+
+output "ansible_hosts" {
+  description = <<-EOT
+    Render a basic Ansible hosts template which can then be used as a form of
+    dynamic inventory by the playbooks
+  EOT
+
+  value = templatefile(
+    "templates/ansible_hosts.yaml.tftpl",
+    {
+      host_ip_addr = {
+        for instance, attributes in vultr_instance.host :
+        lower(regex("\\w+", attributes.os)) => attributes.main_ip
+      }
+    }
+  )
 }
